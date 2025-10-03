f4869bcfd39a1f8d813c70813620b127bd5368ba
Dimpho Matea <125918769+CYBER-SKUVA@users.noreply.github.com>
2025-10-03 02:54:45 +0200

    feat: I added the basement model and it has walls that block character from walking through. The character now rotates/ turns with the camera, we always see the character POV.

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    target: "esnext", // Ensure compatibility with modern JS features
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Enable WASM support
+      supported: {
+        "top-level-await": true,
+      },
+    },
+  },
+  assetsInclude: ["**/*.wasm", "**/*.glb", "**/*.fbx", "**/*.gltf"],
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+});
