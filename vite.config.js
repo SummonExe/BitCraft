d7cf29348cd65185b34eb083a0464cfd69422d4a
Lebo-Kharafu <Kharafu23@gmail.com>
2025-10-15 23:10:29 +0200

    feat: temp build fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+	plugins: [topLevelAwait()],
+	publicDir: "./public",
+	base: "./",
 	build: {
-		target: "esnext", // Ensure compatibility with modern JS features
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			// Enable WASM support
-			supported: {
-				"top-level-await": true,
+		rollupOptions: {
+			input: {
+				main: "./index.html",
+				stage1: "./src/pages/stage1/stage1.html",
+				stage2: "./src/pages/stage2/stage2.html",
+				stage3: "./src/pages/stage3/stage3.html",
+				credits: "./src/pages/credits/credits.html",
 			},
 		},
 	},
@@ -25,6 +29,9 @@ export default defineConfig({
 	server: {
 		fs: {
 			strict: false,
+		},
+		mimeTypes: {
+			"application/javascript": ["js", "mjs"],
 		},
 	},
 });
